62de27e7ebe1c9f292684829b68045ed185b58cc
Oliver Krentzman <67602867+oliver-k28@users.noreply.github.com>
2022-08-27 20:31:40 -0500

    Add files via upload

---
+++ grading_program.py
@@ -0,0 +1,28 @@
+# program that converts their scores to grades.
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+#TODO-1: Create an empty dictionary called student_grades.
+
+student_grades = {}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+
+for student in student_scores:
+    score = student_scores[student]
+    if score >= 91:
+      student_grades[student] = "Outstanding"
+    elif score >= 81 and score <= 90:
+      student_grades[student] = "Exceeds Expectations"
+    elif score >= 71 and score <=80:
+      student_grades[student] = "Acceptable"
+    elif score <= 70:
+      student_grades[student] = "Fail"   
+
+print(student_grades)

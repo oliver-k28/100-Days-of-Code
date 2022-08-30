66520a3ec953c49a19442f78c83e508716b1f01d
Oliver Krentzman <67602867+oliver-k28@users.noreply.github.com>
2022-08-29 22:13:24 -0500

    Add files via upload

---
+++ travel_log.py
@@ -0,0 +1,27 @@
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log.
+
+def add_new_country(country, num_visits, cities):
+    new_country = {}
+    new_country["country"] = country 
+    new_country["visits"] = num_visits
+    new_country["cities"] = cities
+    travel_log.append(new_country)
+
+ 
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
